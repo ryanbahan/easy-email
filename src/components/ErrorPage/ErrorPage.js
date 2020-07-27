2408c87f6ce0ef65d4492009ebf97aa1eb216b2f
Ryan Bahan <ryan@ryanbahan.com>
2020-07-27 12:04:19 -0600

    Move redux actions and reducers to own folder

--- src/components/ErrorPage/ErrorPage.js
+++ src/components/ErrorPage/ErrorPage.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
-import { clearError } from '../../utils/actions';
+import { clearError } from '../../redux/actions';
 import './ErrorPage.css';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
